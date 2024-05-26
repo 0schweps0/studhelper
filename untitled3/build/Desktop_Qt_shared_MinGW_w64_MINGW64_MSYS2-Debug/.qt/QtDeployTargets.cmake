3172f70950fc0d2895f274d28d28606370a85287
Igorёha <fort.fort.03@mail.ru>
2024-05-26 16:11:14 +0300

    123

---
+++ untitled3/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_untitled3_FILE C:/Users/EDELWEISS PC/Documents/untitled3/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/untitled3.exe)
+set(__QT_DEPLOY_TARGET_untitled3_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
