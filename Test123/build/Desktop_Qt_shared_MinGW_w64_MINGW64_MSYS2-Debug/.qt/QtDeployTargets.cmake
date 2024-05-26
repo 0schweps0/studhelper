921e6d4757bf628fbfe05a035f6f97f8b18badce
Igorёha <fort.fort.03@mail.ru>
2024-05-26 16:44:02 +0300

    123

---
+++ Test123/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Test123_FILE C:/Users/EDELWEISS PC/Documents/GitHub/studhelper/Test123/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/Test123.exe)
+set(__QT_DEPLOY_TARGET_Test123_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
