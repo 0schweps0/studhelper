50d4458dfe843651e7f420e228d6ecf4b2f54a63
Igorёha <fort.fort.03@mail.ru>
2024-05-26 16:32:27 +0300

    все нахуй

--- test1.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by EDELWEISS PC on 25.05.2024.
-//
-123121
-askdfjkaldjf
-12312
--- test3/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_test3_FILE C:/Users/EDELWEISS PC/Documents/GitHub/studhelper/test3/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/test3.exe)
-set(__QT_DEPLOY_TARGET_test3_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
