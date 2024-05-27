6be41e2937395c1e7fda91276c0ee158b8867eea
0schweps0 <122373099+0schweps0@users.noreply.github.com>
2024-05-27 14:02:54 +0300

    222

---
+++ untitled52/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/CMakeFiles/untitled52_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled52_autogen"
+  "untitled52_autogen/include/ui_mainwindow.h"
+  "untitled52_autogen/mocs_compilation.cpp"
+  "untitled52_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/untitled52_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
