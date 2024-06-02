1693f7fd1db109d51763de7c74334663c0765032
0schweps0 <122373099+0schweps0@users.noreply.github.com>
2024-06-02 23:36:31 +0300

    delete all others

--- Test123/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Test123_FILE C:/Users/glebg/OneDrive/Documents/GitHub/studhelper/Test123/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/Test123.exe)
-set(__QT_DEPLOY_TARGET_Test123_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
