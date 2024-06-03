54dfd1a3310fbb9139c3d9b805dd20a2ed761130
0schweps0 <122373099+0schweps0@users.noreply.github.com>
2024-06-03 13:40:54 +0300

    where

--- untitled52/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
+++ untitled52/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
@@ -1,2 +1,2 @@
-set(__QT_DEPLOY_TARGET_untitled52_FILE C:/Users/EDELWEISS PC/Documents/GitHub/studhelper/untitled52/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/untitled52.exe)
-set(__QT_DEPLOY_TARGET_untitled52_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
+set(__QT_DEPLOY_TARGET_untitled52_FILE D:/github/studhelper/untitled52/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/untitled52.exe)
+set(__QT_DEPLOY_TARGET_untitled52_RUNTIME_DLLS D:/msys64/mingw64/bin/Qt6Widgets.dll;D:/msys64/mingw64/bin/Qt6Gui.dll;D:/msys64/mingw64/bin/Qt6Core.dll)
