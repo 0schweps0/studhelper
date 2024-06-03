80e7e1716775f053f3a3a653f96f2ab4ddf53816
0schweps0 <122373099+0schweps0@users.noreply.github.com>
2024-06-03 03:23:20 +0300

    2.0 last commit

--- untitled52/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/qtcsettings.cmake
+++ untitled52/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/qtcsettings.cmake
@@ -1,3 +1,3 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_CXX_COMPILER" "D:/msys64/mingw64/bin/g++.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/msys64/mingw64/bin/g++.exe" CACHE "STRING" "" FORCE)
